3aef752e074da4eb9ed2322f4a1e748c394b98ac
rickyfriady <friadi.ricki@gmail.com>
2023-03-09 16:49:11 +0700

    make mongo schema and model, make autentication controller, make random and autentication function utils

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import { authentication, random } from 'utils';
+import { createUser, getUserByEmail } from './../db/User';
+
+export const register = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password, username } = req.body;
+
+    if (!email || !password || !username) {
+      return res.sendStatus(400);
+    }
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) return res.sendStatus(404);
+
+    const salt = random();
+    const user = await createUser({
+      email,
+      username,
+      authentication: {
+        salt,
+        password: authentication(salt, password),
+      },
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,11 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
 import http from 'http';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 8800;
+const MONGO_URL = 'mongodb+srv://ricki:mongodb@cluserts.m0ftvpb.mongodb.net/?retryWrites=true&w=majority';
 
 app.use(
   cors({
@@ -23,3 +25,7 @@ const server = http.createServer(app);
 server.listen(PORT, () => {
   console.log(`server running on http://localhost:${PORT}/`);
 });
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
