d26d9fa306f7414aac36151cb01682d16cd80b24
rickyfriady <friadi.ricki@gmail.com>
2023-03-10 17:33:01 +0700

    make middleware isOwner and add new controller users

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,6 +1,21 @@
 import express from 'express';
-import { merge } from 'lodash';
+import { get, merge } from 'lodash';
 import { getUserBySessionToken } from './../db/User';
+
+export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  try {
+    const { id } = req.params;
+
+    const currentUserId = get(req, 'identity._id') as String;
+    if (!currentUserId) return res.sendStatus(400);
+    if (currentUserId.toString() !== id) return res.sendStatus(403);
+
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
 
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
   try {
